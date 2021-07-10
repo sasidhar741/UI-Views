ab2d8c0fb44c5306363aba8a9766347e3b0b2c95
sasidhar <sasidhar741@gmail.com>
2021-07-10 16:04:42 +0530

    UI Views with autoLayout

--- UI-Views/ViewController.swift
+++ UI-Views/ViewController.swift
@@ -11,9 +11,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        datePicker.locale = .current
+        datePicker.date = Date()
+        datePicker.backgroundColor = .black
+        datePicker.textColor = .white
+        
+}
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        
+        
     }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("Button pressed")
+    }
+    
+    @IBAction func scPressed(_ sender: UISegmentedControl) {
+        //based on selected segment change the background
+    }
+    
+    @IBAction func textField(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func horizontalSlider(_ sender: UISlider) {
+        
+    }
+    
 
+    
+    @IBAction func `switch`(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func stepper(_ sender: UIStepper) {
+        textLabel.text = String(Int(sender.value))
+    }
+    
+}
+extension UIDatePicker {
 
+var textColor: UIColor? {
+    set {
+        setValue(newValue, forKeyPath: "textColor")
+    }
+    get {
+        return value(forKeyPath: "textColor") as? UIColor
+    }
+  }
 }
 
